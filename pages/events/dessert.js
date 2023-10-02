e29b1bc3b5261d01d02264680dc4054cb7a0cca6
amirbar002 <amirbar002@gmail.com>
2023-10-02 15:03:14 +0300

    we work

--- pages/events/dessert.js
+++ pages/events/dessert.js
@@ -1,54 +1,63 @@
-import { useEffect, useState } from 'react';
 import AllEvents from '../../src/app/home/meat';
-import axios from 'axios';
-import Image from 'next/legacy/image';
+import axios from "axios";
+import Image from "next/legacy/image";
+import { useEffect, useState } from 'react';
 
-const EventsPage = ({ initialData }) => {
-  const [data, setData] = useState(initialData);
+const EventsPage = ({ data }) => {
 
-  const getCard = async () => {
+  const getcard = async () => {
     try {
-      const res = await axios.get('https://cooking-book-77339b050a7a.herokuapp.com/recipes/4');
-      if (JSON.stringify(res.data) !== JSON.stringify(data)) {
-        setData(res.data);
-        console.log('New data received from getStaticProps');
-      } else {
-        console.log('Data remains the same');
+      const res = await axios.get( "https://cooking-book-77339b050a7a.herokuapp.com/recipes/5")
+      if((JSON.stringify(res.data) !== JSON.stringify(data)) ){
+        data = res.data
+        console.log("res.data");
+      }else{
+        console.log('data from getStaticProps');
       }
+     
+
     } catch (error) {
       console.log(error);
     }
-  };
-
+    getcard()
+  }
   useEffect(() => {
     getCard();
-  }, []); 
-
+  }, [])
+ 
   return (
     <>
-      <div
+    <div
         style={{
           zIndex: -1,
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-          objectFit: 'cover',
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          objectFit: "cover",
         }}
       >
-        <Image src="/../img9.jpeg" alt="background" layout="fill" quality={100} />
+        <Image
+          src="/../img5.jpeg"
+          alt="background"
+          layout="fill"
+          quality={100}
+        />
       </div>
-      <AllEvents data={data} />
-    </>
+  <AllEvents data={data} />
+  </>
   );
+ 
+  
 };
 
 export default EventsPage;
 
 export async function getStaticProps() {
-  const res = await axios.get('https://cooking-book-77339b050a7a.herokuapp.com/recipes/4');
+  const res = await axios.get("https://cooking-book-77339b050a7a.herokuapp.com/recipes/5")
   return {
     props: {
-      initialData: res.data,
+      data: res.data,
     },
   };
 }
+
